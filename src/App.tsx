daa58ac182ba9611359b641c66eefce4a2d22a69
yaoweifeng <1040123159@qq.com>
2021-01-22 16:59:12 +0800

    使用react-router

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,67 @@ import React from 'react';
 import styled from 'styled-components'
 import './App.css';
 
-const Button = styled.button`
-  color:red;
-`
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Button>nihao </Button>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Redirect exact from='/' to="Money" />
+          <Route path='*'>
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+
+function NotFound() {
+  return <h2>找不到页面</h2>;
+}
 export default App;
